87f309acd1bd0dcef8640a835b43493e4206fd4e
Alex Demidoff <alex@demidoff.me>
2025-06-20 00:01:58 +0300

    fix: special chars generation

--- valkey/sentinel/cleanup.sh
+++ valkey/sentinel/cleanup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
 
-# docker exec -it pmm-server pmm-admin remove valkey valkey-svc1 || :
-# docker exec -it pmm-server pmm-admin remove valkey valkey-svc2 || :
-# docker exec -it pmm-server pmm-admin remove valkey valkey-svc3 || :
+docker exec -it pmm-server pmm-admin remove valkey valkey-primary-svc || :
+docker exec -it pmm-server pmm-admin remove valkey valkey-replica1-svc || :
+docker exec -it pmm-server pmm-admin remove valkey valkey-replica2-svc || :
+docker exec -it pmm-server pmm-admin remove valkey sentinel1-svc || :
+docker exec -it pmm-server pmm-admin remove valkey sentinel2-svc || :
+docker exec -it pmm-server pmm-admin remove valkey sentinel3-svc || :
 
-docker rm -vf valkey-primary valkey-replica-1 valkey-replica-2 || true
-docker rm -vf sentinel-1 sentinel-2 sentinel-3 || true
+docker rm -vf valkey-primary valkey-replica-1 valkey-replica-2 || :
+docker rm -vf sentinel-1 sentinel-2 sentinel-3 || :
 
-docker volume rm -f valkey-primary-data valkey-replica-1-data valkey-replica-2-data || true
+docker volume rm -f valkey-primary-data valkey-replica-1-data valkey-replica-2-data || :
 
 rm -rf "$HOME/valkey"
-
